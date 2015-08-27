20bd8598e908ea96c7165779f6cf1dc18ca5a7f5
SUDARSAN <sudarsan.x.james@verizon.com>
2015-08-27 06:07:31 -0700

    mysql datasource

--- src/main/java/com/codered/EditorBean.java
+++ src/main/java/com/codered/EditorBean.java
@@ -5,10 +5,11 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean(name = "editor")
 public class EditorBean {
 
-	private String value = "This editor is provided by Prim";
+	private String value = "This editor is provided by Hackathon guys";
 
 	public String getValue() {
 		System.out.println("testest1213131");
+		MySqlDataSource.getConnection();
 		return value;
 	}
 
