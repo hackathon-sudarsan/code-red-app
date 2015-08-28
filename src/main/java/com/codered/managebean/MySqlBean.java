68fbe0123db0abc62809b40d0664155f10bac1cd
SUDARSAN <sudarsan.x.james@verizon.com>
2015-08-28 12:55:27 +0530

    read query added

--- src/main/java/com/codered/managebean/MySqlBean.java
+++ src/main/java/com/codered/managebean/MySqlBean.java
@@ -1,8 +1,12 @@
 package com.codered.managebean;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -40,6 +44,7 @@ public class MySqlBean {
 
 	Connection conn = null;
 	Statement stmt = null;
+
 	public int executeQuery() {
 		int rows = 0;
 		try {
@@ -50,7 +55,8 @@ public class MySqlBean {
 			rows = stmt.executeUpdate(this.query);
 		} catch (SQLException se) {
 			// Handle errors for JDBC
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Fatal!", "System Error"));
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_FATAL, "Fatal!", "System Error"));
 			se.printStackTrace();
 		} catch (Exception e) {
 			// Handle errors for Class.forName
@@ -76,6 +82,57 @@ public class MySqlBean {
 		FacesContext.getCurrentInstance().addMessage(null,
 				new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "PrimeFaces Rocks."));
 		return rows;
+	}
+
+	public void executeReadQuery() {
+		List<String> ls = new ArrayList<String>();
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = MySqlDataSource.getConnection();
+			stmt = conn.createStatement();
+			System.out.println("Query : " + this.query);
+			ResultSet rs = stmt.executeQuery(this.query);
+			ResultSetMetaData meta = rs.getMetaData();
+			int colCount = meta.getColumnCount();
+			
+			while (rs.next()) {
+				for (int col = 1; col <= colCount; col++) {
+					Object value = rs.getObject(col);
+					if (value != null) {
+						System.out.print(value.toString());
+						ls.add(value.toString());
+					}
+				}
+			}
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_FATAL, "Fatal!", "System Error"));
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "PrimeFaces Rocks."));
+			e.printStackTrace();
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+			} // nothing we can do
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			} // end finally try
+		} // end try
+
+		
+		this.setErrMsg("Refere lo: "+ls);
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "PrimeFaces Rocks."));
+	
 	}
 
 	@Override
